b471d5112ca4a0c60718d40303d8ffb3d71489c1
Hunter_Jack <guoziyu321@qq.com>
2016-10-09 22:02:47 +0800

    Create window.java

---
+++ window.java
@@ -0,0 +1,19 @@
+/*
+	date:2016.10.05
+	author:Hunter
+	description:
+*/
+import java.util.Scanner;
+import java.io.*;
+
+public class window{
+	public static void main(String argv[]){
+		//variables-------------------------------------------------------//
+		//Scanner reader=new Scanner(System.in);
+		//serverWindow login1=new serverWindow("Server",100,100,500,250);
+													//x  ,y  ,w  ,h
+		//operations------------------------------------------------------//
+		serverWindow login1=new serverWindow("Server",100,100,950,630);
+		
+	}
+}
