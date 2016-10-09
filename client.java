8c505f6bc1fd9616d51b0fada9a3bd3258ca546e
Hunter_Jack <guoziyu321@qq.com>
2016-10-09 22:04:43 +0800

    Create client.java

---
+++ client.java
@@ -0,0 +1,19 @@
+/*
+	date:2016.10.09
+	author:Hunter
+	description:	creating for file sharing server's GUI
+*/
+import java.util.Scanner;
+import java.io.*;
+
+public class client{
+	public static void main(String argv[]){
+		//variables-------------------------------------------------------//
+		//Scanner reader=new Scanner(System.in);
+		//serverWindow login1=new serverWindow("Server",100,100,500,250);
+													//x  ,y  ,w  ,h
+		//operations------------------------------------------------------//
+		clientWindow login1=new clientWindow("Client :",100,100,950,630);
+		
+	}
+}
